4b6487bdcb8c6ee192069cd0c24d1a66841685b3
Rajan Kumar <rajank@argusoft.com>
2026-06-22 18:29:06 +0530

    Changed welcome message

--- chatApp/client/src/components/AuthPage.jsx
+++ chatApp/client/src/components/AuthPage.jsx
@@ -1,19 +1,19 @@
-import axios from "axios";
+import axios from 'axios';
 
 const AuthPage = (props) => {
   const onSubmit = (e) => {
     e.preventDefault();
     const { value } = e.target[0];
     axios
-      .post("http://localhost:3001/authenticate", { username: value })
+      .post('http://localhost:3001/authenticate', { username: value })
       .then((r) => props.onAuth({ ...r.data, secret: value }))
-      .catch((e) => console.log("Auth Error", e));
+      .catch((e) => console.log('Auth Error', e));
   };
 
   return (
     <div className="background">
       <form onSubmit={onSubmit} className="form-card">
-        <div className="form-title">Welcome 👋</div>
+        <div className="form-title">Welcome User👋</div>
 
         <div className="form-subtitle">Set a username to get started</div>
 
